eca333b589193c29d8066ffc7a6fe34dc8286dca
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-14 03:11:35 +0100

    add constrains to image

--- auctions/models.py
+++ auctions/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
-
+from PIL import Image
 
 class User(AbstractUser):
     pass
@@ -21,10 +21,18 @@ class Listing(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField(max_length=200, blank='')
     price = models.IntegerField()
-    image = models.ImageField(default='', upload_to='listings_images')
+    image = models.ImageField(default='default.jpg', upload_to='listings_images')
     category = models.CharField(max_length=15, choices=CATEGORIES, default='FA')
     date_created = models.DateTimeField(default=timezone.now)
 
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 class Bid(models.Model):
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing_user')
