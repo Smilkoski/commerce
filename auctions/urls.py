fa9746a332ea3760567296ea3dc3035c94ff85cb
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-14 01:52:01 +0100

    change routes

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    ListingListView,
+    ListingCreateView,
+    login_view,
+    logout_view,
+    register,
+
+)
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("", ListingListView.as_view(), name="index"),
+    path("create/", ListingCreateView.as_view(), name="create"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register, name="register")
 ]
