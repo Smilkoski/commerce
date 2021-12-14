51acadd2a78f9cdd1c5cf007768e86556458574b
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-14 19:27:20 +0100

    add detail view and watchlist in urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -5,18 +5,22 @@ from commerce import settings
 from .views import (
     ListingListView,
     ListingCreateView,
+    ListingDetailView,
     login_view,
     logout_view,
     register,
-
+    WatchlistListView,
 )
 
 urlpatterns = [
     path("", ListingListView.as_view(), name="index"),
     path("create/", ListingCreateView.as_view(), name="create"),
+    path("detail/<int:pk>", ListingDetailView.as_view(), name="detail_listing"),
+    path("watchlist/<int:pk>", WatchlistListView.as_view(), name="watchlist"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("register/", register, name="register")
+
 ]
 
 if settings.DEBUG:
