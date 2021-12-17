c03001d4f244f5dfc34b5fb16a847b3fbcd0652f
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-17 04:19:38 +0100

    add CommentForm to forms.py

--- auctions/forms.py
+++ auctions/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from .models import (
     Bid,
+    Comment,
 )
 
 
@@ -11,3 +12,11 @@ class BidForm(forms.ModelForm):
     class Meta:
         model = Bid
         fields = ['price']
+
+
+class CommentForm(forms.ModelForm):
+    comment = forms.Textarea()
+
+    class Meta:
+        model = Comment
+        fields = ['comment']
